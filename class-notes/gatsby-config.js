9ed6228f986097f318480ff91f7f3c2f05c72c27
alberto <aarroyo@altusacademy.org>
2022-10-19 18:22:01 +0000

    Update gatsby-config.js

--- class-notes/gatsby-config.js
+++ class-notes/gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
-        name: `blog`,
+        name:alberto's`blog`,
       },
     },
     {
